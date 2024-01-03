151478dd6b2e4d8293c5ed08be109f1ca334c902
hellozo0 <ahju0011@gmail.com>
2024-01-03 23:42:44 +0900

    실수로 한줄 통으로 삭제

--- CICD_TEST/src/main/java/com/hellozo0/CICD_TEST/controller/HeartController.java
+++ CICD_TEST/src/main/java/com/hellozo0/CICD_TEST/controller/HeartController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HeartController {
 
-    private final HeartService heartService;
 
     @Operation(summary = "책 좋아요/취소", description = "책 좋아요/취소 기능 API입니다.")
     @ApiResponses({
