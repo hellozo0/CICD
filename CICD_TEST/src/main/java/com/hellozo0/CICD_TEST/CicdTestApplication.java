b6414c483cce073133cf4dd88c2a301baa130ffa
hellozo0 <ahju0011@gmail.com>
2024-01-03 21:53:55 +0900

    초기세팅

---
+++ CICD_TEST/src/main/java/com/hellozo0/CICD_TEST/CicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.hellozo0.CICD_TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
---
+++ CICD_TEST/src/test/java/com/hellozo0/CICD_TEST/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellozo0.CICD_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
